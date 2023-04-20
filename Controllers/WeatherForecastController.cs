5e59d88b5144901ca7da28f0bd6a9e9d62904e9d
Björn Noctiluca <bjorn.noctiluca@outlook.com>
2023-04-20 10:20:21 +0200

    Set up for initial deploy

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -19,14 +19,9 @@ public class WeatherForecastController : ControllerBase
     }
 
     [HttpGet(Name = "GetWeatherForecast")]
-    public IEnumerable<WeatherForecast> Get()
+    public string Get()
     {
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-        {
-            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            TemperatureC = Random.Shared.Next(-20, 55),
-            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        })
-        .ToArray();
+        _logger.LogInformation("It's working");
+        return "hello world";
     }
 }
