f66b8dd5500b266709105959a36f4baaf2aa63d0
ThePinkBear <bjorn.noctiluca@outlook.com>
2023-04-29 11:27:11 +0200

    Add Image class

---
+++ Models/Image.cs
@@ -0,0 +1,10 @@
+public class Image
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public string? Description { get; set; }
+    public byte[]? Data { get; set; }
+    public string? ContentType { get; set; }
+    public int Height { get; set; }
+    public int Width { get; set; }
+}
